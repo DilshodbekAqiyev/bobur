0d9ba8da705499361cfe8b9f3854de35349a17d3
Dilshodbek <dilshodjonaqiyev@gmail.com>
2024-02-12 22:16:04 +0500

    ads

--- src/components/pages/home/menu/menu.jsx
+++ src/components/pages/home/menu/menu.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Button, Heading3, Heading5 } from "../../../common";
-import { plantTypes } from "../../../../constants/plantTypes";
+import React from 'react'
+import { Button, Heading3, Heading5 } from '../../../common'
+import { plantTypes } from '../../../../constants/plantTypes'
 
 export const Menu = ({ styles }) => {
   return (
@@ -8,10 +8,18 @@ export const Menu = ({ styles }) => {
       <div className={`bg-grey dark:bg-slate-800 p-[24px] `}>
         <Heading3>Categories</Heading3>
         <ul className="flex flex-col ml-[12px] mt-[10px]">
-          {plantTypes.map((type,i)=>i<9&&<li className="flex items-center justify-between text-2 text-black  dark:text-white hover:text-green hover:font-bold transition leading-2 font-normal py-[10px]">
-            <p>{type.type}</p>
-            <p>({type.count})</p>
-          </li>)}
+          {plantTypes.map(
+            (type, i) =>
+              i < 9 && (
+                <li
+                  key={type.id}
+                  className="flex items-center justify-between text-2 text-black  dark:text-white hover:text-green hover:font-bold transition leading-2 font-normal py-[10px]"
+                >
+                  <p>{type.type}</p>
+                  <p>({type.count})</p>
+                </li>
+              )
+          )}
         </ul>
         <Heading3>Price Range</Heading3>
         <div className=" ml-[12px] mt-[10px] mb-[45px]">
@@ -23,20 +31,25 @@ export const Menu = ({ styles }) => {
         </div>
         <Heading3>Size</Heading3>
         <ul className="ml-[12px]">
-          {plantTypes.map((type,i)=>i>8&&<li className="flex items-center justify-between text-2 text-black dark:text-white hover:text-green hover:font-bold transition leading-2 font-normal py-[10px]">
-            <p>{type.type}</p>
-            <p>({type.count})</p>
-          </li>)}
+          {plantTypes.map(
+            (type, i) =>
+              i > 8 && (
+                <li
+                  key={type.id}
+                  className="flex items-center justify-between text-2 text-black dark:text-white hover:text-green hover:font-bold transition leading-2 font-normal py-[10px]"
+                >
+                  <p>{type.type}</p>
+                  <p>({type.count})</p>
+                </li>
+              )
+          )}
         </ul>
       </div>
       <div className="bg-[#96eca749] flex flex-col items-center">
         <img src="/img/superSale.png" alt="" />
-        <Heading3 styles='text-7 mt-[7px]'>UP TO 75% OFF</Heading3>
-        <img
-          src="/img/flower.png"
-          alt=""
-        />
+        <Heading3 styles="text-7 mt-[7px]">UP TO 75% OFF</Heading3>
+        <img src="/img/flower.png" alt="" />
       </div>
     </div>
-  );
-};
+  )
+}
