5d3168a247c9882cce3ee2940a624860ea27473a
DiyorbekChoriyev <diyorbekchoriyev122@gmail.com>
2024-02-14 11:51:06 +0500

    afsdc

---
+++ src/components/pages/home/ruboilar/ruboilar.jsx
@@ -0,0 +1,98 @@
+import { Heading3, Heading4 } from "../../../common"
+
+export const Ruboiy=()=>{
+    return(
+        <div>
+            <Heading3 styles="text-[40px] text-green ml-[20px] my-[60px]">Zahriddin Muhammad Bobur ruboiylar to`plami </Heading3>
+            <div className="flex mt-[50px] ml-[20px]">
+            <p>
+                <img className="w-[95%]" src="/img/ruboiy.jpg" alt="" />
+            </p>
+            <p className="w-[50%] ml-[20px] text-[18px]">
+                Har kimki vafo qilsa vafo topgusidur,
+                Har kimki jafo qilsa jafo topgusidur,
+                Yahshi kishi yomonlig’ ko’rmagay hargiz,
+                Ha kimki yomon bo’lsa jazo topgusidur!
+                <p>*****</p>
+                Jondin seni ko’p sevarmen, ey umri aziz,
+                Sondin seni ko’p sevarmen, ey umri aziz,
+                Har neniki sevmak andin ortiq bo’lmaz,
+                Andin seni ko’p sevarmen, ey umri aziz.
+                <p>*****</p>
+                <p>
+                Sen gulsenu men haqir bulbuldirmen,
+                Sen shu’lasen,ul shu’lag’a men kuldirmen.
+                Nisbat yo’qdur,deb ijtinob aylamakim,
+                Shohmen elga,vale senga quldurmen.
+                </p>
+                <p>*****</p>
+                <p>
+                Tuz oh, Zahiriddin Muhammad Bobur,
+                Yuz oh, Zahiriddin Muhammad Bobur,
+                Sarrishtai ayshdin ko’ngulni zinhor
+                Uz oh, Zahiriddin Muhammad Bobur.
+                </p>
+                <p>*****</p>
+                <p>
+                Ahbob, yig’ilmoqni farog’at tutungiz,
+                Jamiyatingiz borini davlat tutungiz.
+                Chun gardishi charx bu durur, tengri uchun,
+                Bir birini necha kuni g’animat tutungiz.
+                </p>
+            </p>
+            </div>
+            <div className="flex mt-[30px] ml-[20px] gap-[10px] mb-[30px]">
+            <Heading4 styles=" text-[30px] text-[black]">Tavsiya etamiz o`git qing:</Heading4>
+            <a className="border-0 border-white border-b-indigo-500 text-[30px] w-[900]" href="https://bilimlar.uz/zahriddin-muhammad-bobur-sara-gazallari/"> Zahriddin Muhammad Bobur sara g`azallar to`plami</a>
+            </div>
+            <p className="w-[32%] ml-[20px] text-[18px]">
+                 Yod etmas emish kishini g’urbatda kishi,
+                Shod etmas emish ko’ngulni mehnatda kishi.
+                Ko’nglim bu g’ariblikda shod o’lmadi, oh,
+                G’urbatda sevinmas emish, albatta, kishi.
+            </p>
+            <p className="ml-[20px]">*****</p>
+            <p className="w-[30%] ml-[20px] text-[18px]">
+                Taqdir yuki boshimga balolig’ bo’ldi,
+                Har neniki ayladim hatolig’ bo’ldi,
+                Oz yurtni qo’yib Hind sori yuzlandim,
+                Yo Rab netayin, bu ne yuz qarolig’ bo’ldi
+            </p>
+            <p className="ml-[20px]">*****</p>
+            <p className="w-[30%] ml-[20px] text-[18px]">
+                Jono, bitigingki, bo‘ldi marqum manga,
+                Mehnat bila g‘amni qildi ma’dum manga.
+                Ko‘rgach ani garchi shod behad bo‘ldum,
+                Mazmuni, vale, bo‘lmadi ma’lum manga
+            </p>
+            <p className="ml-[20px]">*****</p>
+            <p className="w-[30%] ml-[20px] text-[18px]">
+                Ne ishratu aysh uchun mayi nob manga,
+                Ne toat uchun go‘shai mehrob manga,
+                Ne fisq qilurg‘a bordur asbob manga,
+                Ne zohid o‘lurg‘a toqatu tob manga.
+            </p>
+            <Heading4 styles="mt-[30px] text-[30px] ml-[20px] text-[green] mb-[30px] ">
+            Zahiriddin Muhammad Bobur Tuyuqlari
+            </Heading4>
+            
+            <p className="w-[30%] ml-[20px] text-[18px]">
+                Vasldin so‘z derga yo‘q yoro manga,
+                Hajr aro rahm aylagil, yoro, manga.
+                O‘qung etti ko‘p yamon yoro manga,
+                Marhami lutfung bila yoro manga.
+            <p>*****</p>
+                <p>
+                Qadimni firoq mehnati yo qildi,
+                Ko‘nglum g’am-u anduh o‘tig’a yoqildi.
+                Holimni sabog’a aytib erdim, ey gul,
+                Bilmon, sanga sharh qilmadi, yo qildi.
+                </p>
+            </p>
+            <div className="flex mt-[30px] ml-[20px] gap-[10px] mb-[30px]">
+            <Heading4 styles=" text-[30px] text-[black]">Tavsiya etamiz:</Heading4>
+            <a className="border-0 border-white border-b-indigo-500 text-[30px] w-[900]" href="https://bilimlar.uz/sevgi-muhabbat-haqida-eng-sara-sherlar-toplami/" >Sevgi muhabat haqida sara sherlar to`plami</a>
+            </div>
+        </div>
+    )
+}
